75f1745ac12441ecda2b21c3e047fd62e6b426f8
Kiran9527 <43056819+Kiran952@users.noreply.github.com>
2021-05-18 16:24:53 +0530

    Test Commit

--- Love Pattern.py
+++ Love Pattern.py
@@ -1,11 +1,11 @@
 from turtle import *
 color("red") #image color
-begin_fill()
+begin_fill()  #befin of the fill
 left(50)
 forward(100)  #distance
 circle(40, 180) #radius
 left(260)
 circle(40, 180)
 forward(100)
-end_fill()
+end_fill()  #end of the fill
 done()
